23bb268e503f5bd75c7b86cd965bcdfe2577f3a1
SAMBOJURAVITEJA <119179698+SAMBOJURAVITEJA@users.noreply.github.com>
2023-09-04 10:54:12 +0530

    Update index.js

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -1,5 +1,10 @@
 import Slider from 'react-slick'
 
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+
 import MovieItem from '../MovieItem'
 
 const MoviesSlider = props => {
